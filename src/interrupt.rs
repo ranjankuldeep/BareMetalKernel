b6146e06dae23eda47ace4f2509f501535de0762
kuldeep ranjan <ranjankuldeep19@gmail.com>
2024-03-26 11:14:40 +0530

    feat: Added Double Fault Handler

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -5,6 +5,7 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt
     };
@@ -20,6 +21,12 @@ extern "x86-interrupt" fn breakpoint_handler(
     println!("EXCEPTION BREAKPOINT\n{:#?}",stack_frame)
 }
 
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame : InterruptStackFrame,
+    _error_code: u64
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
 #[test_case]
 fn test_breakpoint_exception() {
     x86_64::instructions::interrupts::int3();
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ pub extern "C" fn _start() -> ! {
     blog_os::init();
     x86_64::instructions::interrupts::int3();
 
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    };
+
     #[cfg(test)]
     test_main();
 
